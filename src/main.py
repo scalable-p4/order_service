76486d55f80b5aff8ed0e451e50e2d59d0f08c0d
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-12-03 02:52:20 +0700

    connect to Jaeger for traces

--- src/main.py
+++ src/main.py
@@ -22,9 +22,12 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 import logging
 
 # Initialize Tracer for Jaeger
-trace.set_tracer_provider(TracerProvider())
+resource_jaeger = Resource(attributes={
+    SERVICE_NAME: "app-traces"
+})
+trace.set_tracer_provider(TracerProvider(resource=resource_jaeger))
 jaeger_exporter = JaegerExporter(
-    agent_host_name='localhost',
+    agent_host_name='jaeger',
     agent_port=6831,
 )
 trace.get_tracer_provider().add_span_processor(
