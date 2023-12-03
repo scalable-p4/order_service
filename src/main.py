e4d94022f4e78833efa55039eccec76ca5c1c3c6
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-12-04 03:25:24 +0700

    change to use OpenTelemetry Collector

--- src/main.py
+++ src/main.py
@@ -1,55 +1,48 @@
-from fastapi import FastAPI, Response
+import logging
+from fastapi import FastAPI
 from src.database import database
 from src.router import router as router
 from starlette.middleware.cors import CORSMiddleware
-from src.config import app_configs, settings
-import aioredis
-import os
+from src.config import settings
 
 # OpenTelemetry imports for tracing and metrics
 from opentelemetry import trace, metrics
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
-# Prometheus imports
-from prometheus_client import start_http_server
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+# Logging imports
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 
-import logging
+service_name = "order_app"
 
-# Initialize Tracer for Jaeger
-resource_jaeger = Resource(attributes={
-    SERVICE_NAME: "app-traces"
-})
-trace.set_tracer_provider(TracerProvider(resource=resource_jaeger))
-jaeger_exporter = JaegerExporter(
-    agent_host_name='jaeger',
-    agent_port=6831,
-)
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(jaeger_exporter)
-)
+# Initialize TracerProvider for OTLP
+resource = Resource(attributes={SERVICE_NAME: service_name})
+trace_provider = TracerProvider(resource=resource)
+otlp_trace_exporter = OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
+trace_provider.add_span_processor(BatchSpanProcessor(otlp_trace_exporter))
+trace.set_tracer_provider(trace_provider)
 
-# Initialize MeterProvider for Prometheus
-resource = Resource(attributes={
-    SERVICE_NAME: "app-metrics"
-})
-reader = PrometheusMetricReader()
-provider = MeterProvider(resource=resource, metric_readers=[reader])
-metrics.set_meter_provider(provider)
+# Initialize MeterProvider for OTLP
+metric_reader = PeriodicExportingMetricReader(OTLPMetricExporter(endpoint="otel-collector:4317", insecure=True))
+metric_provider = MeterProvider(resource=resource, metric_readers=[metric_reader])
+metrics.set_meter_provider(metric_provider)
 
-meter = metrics.get_meter("example_meter")
+# Initialize LoggerProvider for OTLP
+logger_provider = LoggerProvider(resource=resource)
+otlp_log_exporter = OTLPLogExporter(endpoint="otel-collector:4317", insecure=True)
+logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_log_exporter))
+handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
 
-# Test counter metric
-request_counter = meter.create_counter(
-    name="requests_counter",
-    description="Counts total number of requests",
-    unit="1",
-)
+# Attach OTLP handler to root logger
+logging.getLogger().addHandler(handler)
 
 app = FastAPI()
 
@@ -58,26 +51,32 @@ app.add_middleware(
     allow_origins=settings.CORS_ORIGINS,
     allow_origin_regex=settings.CORS_ORIGINS_REGEX,
     allow_credentials=True,
-    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=settings.CORS_HEADERS,
 )
 
 FastAPIInstrumentor.instrument_app(app)
 
-start_http_server(port=8006)
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Test counter metric
+meter = metrics.get_meter(__name__)
+request_counter = meter.create_counter(
+    name="requests_counter",
+    description="Counts total number of requests",
+    unit="1",
+)
 
 @app.on_event("startup")
 async def startup() -> None:
     await database.connect()
 
-
 @app.get("/")
 async def root():
-    # Increment the counter
     request_counter.add(1)
+    logging.info("Received a request at root endpoint")
     return {"message": "Hello World"}
 
 app.include_router(router, prefix="/api", tags=["API"])
+
+@app.on_event("shutdown")
+def shutdown() -> None:
+    logger_provider.shutdown()
