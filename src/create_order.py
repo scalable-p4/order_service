5a086e36b12311a061ef43872a3d31297469b8a8
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-12-05 14:14:37 +0700

    implement traces,logging,metrics in every process

--- src/create_order.py
+++ src/create_order.py
@@ -10,6 +10,12 @@ from sqlalchemy import create_engine, insert
 from sqlalchemy.orm import sessionmaker
 from src.database import user_order
 import json
+from opentelemetry import trace, metrics
+import logging
+
+tracer = trace.get_tracer(__name__)
+meter = metrics.get_meter(__name__)
+logger = logging.getLogger(__name__)
 
 BROKER_URL = os.getenv("CELERY_BROKER_URL")
 RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
@@ -19,81 +25,111 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
+# Metrics
+order_counter = meter.create_counter(
+    "orders_created",
+    description="Number of orders created",
+    unit="1",
+)
+payment_results_counter = meter.create_counter(
+    "payment_results_waited",
+    description="Counts how many times payment results were waited for",
+    unit="1",
+)
+
+order_commit_counter = meter.create_counter(
+    "orders_committed",
+    description="Counts how many orders were committed",
+    unit="1",
+)
+
+order_rollback_counter = meter.create_counter(
+    "orders_rollback",
+    description="Counts how many orders were rolled back",
+    unit="1",
+)
 
 @celery_app.task(name="create_order")
 def create_order(payload: dict, fn: str):
-    print("fn="+str(fn))
-    print("payload="+str(payload))
-    username: str = payload.get("username")
-    quantity: int = payload.get("quantity")
-    delivery: bool = payload.get("delivery")
-    print("username="+str(username))
-    print("quantity="+str(quantity))
-    print("delivery="+str(delivery))
-    if fn == "order":
-        print("creating order")
-        commit_create_order(username, quantity, delivery)
-        print("order committed"+str(username)+str(quantity)+str(delivery))
-        payment_task = celery_app.send_task("create_payment", queue='q02', args=[payload, "pay"])
-        return waiting_payment_result(payment_task.id)
+    with tracer.start_as_current_span("create_order_task"):
+        logger.info("Creating order", extra={"payload": payload, "function": fn})
+        order_counter.add(1)
 
-    elif fn == "rollback_order":
-        print("rollbacking order")
-        rollback_order(username, quantity, delivery)
-        print("order rollbacked"+str(username)+str(quantity)+str(delivery))
-        return "failed_token_transaction"
-    elif fn == "success_token_transaction":
-        print("success_token_transaction")
-        return "success_token_transaction"
-    else:
-        print("invalid function"+str(username)+str(quantity)+str(delivery))
-        return "failed_token_transactopm"
+        username = payload.get("username")
+        quantity = payload.get("quantity")
+        delivery = payload.get("delivery")
+
+        logger.info(f"Order details: username={username}, quantity={quantity}, delivery={delivery}")
+
+        if fn == "order":
+            logger.info("Creating order in database", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            commit_create_order(username, quantity, delivery)
+            logger.info("Order committed", extra={"username": username, "quantity": quantity, "delivery": delivery})
+
+            payment_task = celery_app.send_task("create_payment", queue='q02', args=[payload, "pay"])
+            return waiting_payment_result(payment_task.id)
+
+        elif fn == "rollback_order":
+            logger.warning("Rolling back order", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            rollback_order(username, quantity, delivery)
+            logger.info("Order rolled back", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            return "failed_token_transaction"
+
+        elif fn == "success_token_transaction":
+            logger.info("Token transaction successful", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            return "success_token_transaction"
+
+        else:
+            logger.error("Invalid function called", extra={"function": fn, "username": username, "quantity": quantity, "delivery": delivery})
+            return "failed_token_transaction"
+
 
 @celery_app.task
 def waiting_payment_result(payment_task_id):
-    time.sleep(0.3)
-    payment_result = AsyncResult(payment_task_id)
-    if payment_result.ready():
-        result_value = payment_result.result
-        print(f"Task result: {result_value}")
-        return result_value
-    else:
-        print("inventory task is still running...")
-        return "inventory task is still running..."
+    with tracer.start_as_current_span("waiting_payment_result"):
+        time.sleep(0.3)
+        payment_result = AsyncResult(payment_task_id)
+        payment_results_counter.add(1)
+
+        if payment_result.ready():
+            result_value = payment_result.result
+            logger.info(f"Payment task result: {result_value}")
+            return result_value
+        else:
+            logger.info("Inventory task is still running...")
+            return "inventory task is still running..."
 
 @celery_app.task
 def commit_create_order(username, quantity, delivery):
-    session = Session()
-    try:
-        insert_query = (
-            insert(user_order)
-            .values({
-                "username": username,
-                "quantity": quantity,
-                "delivery": delivery,
-            })
-        )
-        session.execute(insert_query)
-        session.commit()
-        print("order committed in try")
-    except Exception as e:
-        print(f"Error during database operation: {e}")
-    finally:
-        session.close()
-
+    with tracer.start_as_current_span("commit_create_order"):
+        session = Session()
+        try:
+            insert_query = insert(user_order).values(username=username, quantity=quantity, delivery=delivery)
+            session.execute(insert_query)
+            session.commit()
+            logger.info("Order committed", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            order_commit_counter.add(1)
+        except Exception as e:
+            logger.error("Error during database operation", exc_info=True)
+            raise
+        finally:
+            session.close()
 
 @celery_app.task
 def rollback_order(username, quantity, delivery):
-    session = Session()
-    try:
-        query = "DELETE FROM user_order WHERE (username, delivery, quantity, uuid) = " \
-                "(SELECT username, delivery, quantity, uuid FROM user_order " \
-                "WHERE username = :username AND delivery = :delivery AND " \
-                "quantity = :quantity ORDER BY uuid DESC LIMIT 1);"
-        session.execute(query, {"username": username, "delivery": delivery, "quantity": quantity})
-        session.commit()
-        print("order rollback commited in try")
-    except Exception as e:
-        print(f"Error during database operation: {e}")
-    finally:
-        session.close()
+    with tracer.start_as_current_span("rollback_order"):
+        session = Session()
+        try:
+            query = "DELETE FROM user_order WHERE (username, delivery, quantity, uuid) = " \
+                    "(SELECT username, delivery, quantity, uuid FROM user_order " \
+                    "WHERE username = :username AND delivery = :delivery AND " \
+                    "quantity = :quantity ORDER BY uuid DESC LIMIT 1);"
+            session.execute(query, {"username": username, "delivery": delivery, "quantity": quantity})
+            session.commit()
+            logger.info("Order rollback committed", extra={"username": username, "quantity": quantity, "delivery": delivery})
+            order_rollback_counter.add(1)
+        except Exception as e:
+            logger.error("Error during database operation", exc_info=True)
+            raise
+        finally:
+            session.close()
--- src/router.py
+++ src/router.py
@@ -7,6 +7,18 @@ from celery import Celery
 from sqlalchemy import insert, select
 from celery.result import AsyncResult
 import json
+from opentelemetry import trace, metrics
+import logging
+
+tracer = trace.get_tracer(__name__)
+meter = metrics.get_meter(__name__)
+logger = logging.getLogger(__name__)
+
+order_requests_counter = meter.create_counter(
+    "order_requests",
+    description="Number of order requests",
+    unit="1",
+)
 
 router = APIRouter()
 BROKER_URL = os.getenv("CELERY_BROKER_URL")
@@ -16,34 +28,30 @@ celery_app = Celery('backend', broker=BROKER_URL,
 
 @router.get("/hello", status_code=status.HTTP_200_OK)
 async def say_hi():
-    return {"message": "Hello World auth4"}
+    with tracer.start_as_current_span("get_hello"):
+        logger.info("Hello World request received")
+        return {"message": "Hello World auth4"}
 
 @router.post("/order", status_code=status.HTTP_201_CREATED)
-async def order(
-    request_data: dict
-)-> dict[str, str]:
-    print("recieved order in router v2")
-    fn: str = request_data.get("fn")
-    payload: dict = request_data.get("payload")
-    print("payload="+str(payload))
-    #await commit_create_order(payload.get("username"), payload.get("quantity"), payload.get("delivery"))
-    result = celery_app.send_task("create_order", queue='q01', args=[payload, fn])
-    # celery_app.send_task("create_order", queue='q01', args=[payload, fn])
-    return_result = result.get()
-    print("result="+str(return_result))
-    return {"message": f"order created with result = {return_result}"}
+async def order(request_data: dict) -> dict[str, str]:
+    with tracer.start_as_current_span("post_order"):
+        order_requests_counter.add(1)
+        logger.info("Received order in router v2", extra={"request_data": request_data})
+
+        fn = request_data.get("fn")
+        payload = request_data.get("payload")
+
+        result = celery_app.send_task("create_order", queue='q01', args=[payload, fn])
+        return_result = result.get()
+        logger.info(f"Order result: {return_result}", extra={"return_result": return_result})
+
+        return {"message": f"order created with result = {return_result}"}
 
 async def commit_create_order(username, quantity, delivery):
-    print(" inside commit_create_order")
-    insert_query = (
-        insert(user_order)
-        .values({
-                "username": username,
-                "quantity": quantity,
-                "delivery": delivery,
-            })
-    )
-    await database.fetch_one(insert_query)
+    with tracer.start_as_current_span("commit_create_order"):
+        logger.info("Inside commit_create_order", extra={"username": username, "quantity": quantity, "delivery": delivery})
+        insert_query = insert(user_order).values(username=username, quantity=quantity, delivery=delivery)
+        await database.fetch_one(insert_query)
 
 # def rollback(username, quantity, delivery):
 #     insert_query = (
